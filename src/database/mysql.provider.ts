56b442297212c9b0b70740544b2c1a0c44639883
MatheusValera <lrfp1000@.gmail.com>
2022-10-25 11:28:39 -0300

    refactor: add connection in mysql

--- src/database/mysql.provider.ts
+++ src/database/mysql.provider.ts
@@ -1,14 +1,26 @@
 import { Injectable, Logger } from '@nestjs/common'
+import { createPool, Pool } from 'mysql2/promise'
 
 @Injectable()
 export class MySQLProvider {
   private readonly logger: Logger
+  private readonly pool: Pool
   constructor() {
     this.logger = new Logger('MySQLProvider')
+    this.pool = createPool({
+      host: 'localhost',
+      user: 'root',
+      database: 'cat-products',
+      waitForConnections: true,
+      connectionLimit: 20,
+      queueLimit: 0,
+    })
     this.logger.log('Initialized MySQL')
   }
 
-  getValue(): string {
-    return 'Test!'
+  async getValue(): Promise<any> {
+    const conn = await this.pool.getConnection()
+    const [results] = await conn.query('select * from products')
+    return results
   }
 }
