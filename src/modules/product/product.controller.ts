c8134ed983288d7f347daf8689f0d2eb3259e5e8
MatheusValera <lrfp1000@.gmail.com>
2022-10-25 17:38:37 -0300

    feat: add new route with get product by id

--- src/modules/product/product.controller.ts
+++ src/modules/product/product.controller.ts
@@ -1,5 +1,5 @@
 import { Product } from './product.entity'
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Get, Param } from '@nestjs/common'
 import { ProductService } from './product.service'
 
 @Controller('products')
@@ -8,5 +8,10 @@ export class ProductController {
   @Get()
   async findAll(): Promise<Product[]> {
     return this.productService.findAll()
+  }
+
+  @Get(':id')
+  async findById(@Param('id') id: string): Promise<Product> {
+    return this.productService.findById(id)
   }
 }
